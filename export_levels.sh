8271a9514d0b89465202365b7e8f6bc1ea4ab1e0
AleFunky <alefunkyextra@gmail.com>
2025-09-22 19:51:39 +0200

    Fix music test

--- export_levels.sh
+++ export_levels.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # To add a level, add the name of the .json file (without the extension) to the list of levels
-python3 export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything electromanadventures clubstep electrodynamix leveleasy groundtospace ultimatedestruction thenightmare rose problematic deathmoon everyend test test2
+python3 export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything electromanadventures clubstep electrodynamix leveleasy dorabaebasic7 groundtospace ultimatedestruction thenightmare rose problematic deathmoon everyend test test2
